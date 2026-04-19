84390e6932cd6be3c7fbbea22b2663665b3f1ddd
FragrantResult186 <FragrantResult186@gmail.com>
2026-04-20 00:14:22 +0900

    26/04/20

--- src/util.cpp
+++ src/util.cpp
@@ -158,6 +158,8 @@ QString getBiomeDisplay(int mc, int id)
     case cherry_grove:                  return QApplication::translate("Biome", "Cherry Grove");
     // 1.21.50 (Winter Drop Version TBA)
     case pale_garden:                   return QApplication::translate("Biome", "Pale Garden");
+    // 26.20
+    case sulfur_caves:                  return QApplication::translate("Biome", "Sulfur Caves");
     }
     const char *name = biome2str(mc, id);
     return name ? name : "";
